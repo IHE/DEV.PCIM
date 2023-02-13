4708e7c0b25d7511a0891bef1164dccfc725f540
Mary Jungers <mjungers@ihe.net>
2023-02-13 15:57:12 -0600

    updated gen-html.bat file and name of .md file

--- gen-html.bat
+++ gen-html.bat
@@ -1,1 +1,4 @@
-pandoc -r gfm --template=https://github.com/IHE/publications/wiki/files/ihe_template.html --metadata title="IHE DeIdentification Handbook" --metadata path-prefix="../../" -w html -o index.html DeID-handbook.md
+pandoc -r gfm --template=https://github.com/IHE/publications/wiki/files/ihe_template.html --metadata title="PCIM" --metadata path-prefix="../../" -w html -o index.html IHE_DEV_Suppl_PCIM.md
+
+
+
